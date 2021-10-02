3448ebb256b189e6bea5aa40a8d9876c2616ef02
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-10-02 18:06:32 +0900

    11. 로그아웃 및 닉네임 변경

--- SpotifyLogin/SpotifyLogin/MainViewController.swift
+++ SpotifyLogin/SpotifyLogin/MainViewController.swift
@@ -22,15 +22,18 @@ class MainViewController:UIViewController {
         navigationController?.navigationBar.isHidden = true
         //로그인 했을 경우 파베 인증을 통해 로그인 사용자의 정보 가져오기
         let email = Auth.auth().currentUser?.email ?? "모 고객"
+        print("닉네임 : ", Auth.auth().currentUser?.displayName)
+        let nickName = Auth.auth().currentUser?.displayName ?? email
         welcomeLabel.text = """
 환영합니다.
-\(email)님
+\(nickName)님
 """
         let isEmailSignIn = Auth.auth().currentUser?.providerData[0].providerID == "password"// email사인인지 아닌지
         resetPasswordButton.isHidden = !isEmailSignIn
     }
     
     @IBAction func logoutButtonTapped(_ sender: UIButton){
+        // 로그인 방식과 상관 없이 가능.
         let firebaseAuth = Auth.auth()
         do {
             try firebaseAuth.signOut()
@@ -43,5 +46,16 @@ class MainViewController:UIViewController {
         let email = Auth.auth().currentUser?.email ?? ""
         //비밀번호 재설정 이메일 전송함
         Auth.auth().sendPasswordReset(withEmail: email, completion: nil)
+    }
+    @IBAction func profileUpdateButtonTapped(_ sender: Any) {
+        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+        changeRequest?.displayName = "토끼"
+        changeRequest?.commitChanges { _ in
+            let displayName = Auth.auth().currentUser?.displayName ?? Auth.auth().currentUser?.email ?? "고객"
+            self.welcomeLabel.text = """
+    환영합니다.
+\(displayName)님
+"""
+        }
     }
 }
