96b0e88b4b4f1f8788b3ffa02f252d7a528c4bc6
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-10-06 21:21:26 +0900

    Add gitignore & Initial commit for CreditCardList

---
+++ CreditCardList/CreditCardList/CardListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CardListViewController.swift
+//  CreditCardList
+//
+//  Created by YOONJONG on 2021/10/06.
+//
+
+import UIKit
+class CardListViewController: UITableViewController{
+    // UIViewController + uitableview와 뭐가 다르냐?
+    // 동일. but,
+    // uitableviewcontroller :  uitableview를 구성하는데 필요한 delegate, datasource가 기본 연결되어 있어 별도 선언 필요 없다. root로 uitableview로 가지게 됨.
+    // uiviewcontroller : view를 rootview로 가짐
+    
+    
+    
+    
+    
+}
