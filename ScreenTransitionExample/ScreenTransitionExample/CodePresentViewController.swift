cda44020f17515ada0f2a31029004faa089df128
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-11-10 10:06:31 +0900

    pass data using segueway

--- ScreenTransitionExample/ScreenTransitionExample/CodePresentViewController.swift
+++ ScreenTransitionExample/ScreenTransitionExample/CodePresentViewController.swift
@@ -7,11 +7,17 @@
 
 import UIKit
 
+protocol SendDataDelegate:AnyObject {
+    func sendData(name:String) // name을 넘길 수 있게
+}
+
 class CodePresentViewController: UIViewController {
 
     @IBOutlet weak var nameLabel: UILabel!
     
     var name:String?
+    weak var delegate:SendDataDelegate? // delegate 변수 앞에는 Weak를 붙여줘야. 안붙으면 강한 순한 촴조가 발생하여 메모리 누수가 발생할 수 있음.
+    // delegate 변수 선언을 통해 delegate는 이를 위임할 준비 완료!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +28,8 @@ class CodePresentViewController: UIViewController {
     }
     
     @IBAction func tapBackButton(_ sender: UIButton) {
+        // 데이터 전달받은 뷰컨에서 SendData delegate 프로토콜을 채택하고, delegate를 위임받게 되면 senddata delegate 프로토콜 채택한 이전화면 뷰컨에서 정의된 sendData함수가 실행되게 됨.
+        self.delegate?.sendData(name: "NewGunter")
         self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
     
--- ScreenTransitionExample/ScreenTransitionExample/ViewController.swift
+++ ScreenTransitionExample/ScreenTransitionExample/ViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SendDataDelegate {
 
+    @IBOutlet weak var nameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -25,7 +26,18 @@ class ViewController: UIViewController {
         guard let viewController = self.storyboard?.instantiateViewController(identifier: "CodePresentViewController") as? CodePresentViewController else { return }
         viewController.modalPresentationStyle = .fullScreen
         viewController.name = "gunter"
+        viewController.delegate = self // 이를 통해 delegate를 위임받게 됨
         self.present(viewController, animated: true, completion: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // 전환하려는 VC 인스턴스를 가져오겠다.
+        if let viewController = segue.destination as? SeguePushViewController {
+            viewController.name = "yeah"
+        }
+    }
+    func sendData(name: String) {
+        self.nameLabel.text = name
+        self.nameLabel.sizeToFit()
     }
 }
 
