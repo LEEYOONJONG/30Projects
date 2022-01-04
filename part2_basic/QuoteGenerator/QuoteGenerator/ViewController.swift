149462f4e79b17821046b1f6bb48ae8f6d27f8e5
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-01-04 21:08:39 +0900

    part2_basic

---
+++ part2_basic/QuoteGenerator/QuoteGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuoteGenerator
+//
+//  Created by YOONJONG on 2021/12/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tapQuoteGeneratorButton(_ sender: Any) {
+    
+    }
+    
+}
+
