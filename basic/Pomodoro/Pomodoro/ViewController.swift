1bfabea0a00fd1c99c262e4e0fafd86829d8cedb
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-02-13 00:47:04 +0900

    Add timer logic

--- basic/Pomodoro/Pomodoro/ViewController.swift
+++ basic/Pomodoro/Pomodoro/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var toggleButton: UIButton!
     
     var duration = 60
+    var currentSeconds = 0
+    var timer: DispatchSourceTimer?
     var timerStatus:TimerStatus = .end // default값
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +25,8 @@ class ViewController: UIViewController {
     @IBAction func tapCancelButton(_ sender: Any) {
         switch self.timerStatus{
         case .start, .pause: // 종료 상태로
-            self.timerStatus = .end
-            self.cancelButton.isEnabled = false
-            self.setTimerInfoViewVisible(isHidden: true)
-            self.datePicker.isHidden = false
-            self.toggleButton.isSelected = false
+            
+            self.stopTimer()
         default:
             break
         }
@@ -42,12 +41,16 @@ class ViewController: UIViewController {
             self.setTimerInfoViewVisible(isHidden: false)
             self.datePicker.isHidden = true
             self.cancelButton.isEnabled = true
+            self.currentSeconds = self.duration
+            self.startTimer()
         case .start:
             self.timerStatus = .pause
             self.toggleButton.isSelected = false// 시작
+            self.timer?.suspend()
         case .pause:
             self.timerStatus = .start
             self.toggleButton.isSelected = true // '일시정지'로
+            self.timer?.resume()
         }
     }
     func setTimerInfoViewVisible(isHidden: Bool){
@@ -57,5 +60,33 @@ class ViewController: UIViewController {
     func configureToggleButton(){
         self.toggleButton.setTitle("시작", for: .normal)
         self.toggleButton.setTitle("일시정지", for: .selected)
+    }
+    func startTimer(){
+        if self.timer == nil {
+            self.timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
+            self.timer?.schedule(deadline: .now(), repeating: 1)
+            self.timer?.setEventHandler(handler: { [weak self] in
+                self?.currentSeconds -= 1
+                debugPrint(self?.currentSeconds)
+                if self?.currentSeconds ?? 0 <= 0 {
+                    self?.stopTimer()
+                }
+                
+            })
+            self.timer?.resume()
+        }
+    }
+    func stopTimer(){
+        if self.timerStatus == .pause {
+            self.timer?.resume()
+        }
+        self.timerStatus = .end
+        self.cancelButton.isEnabled = false
+        self.setTimerInfoViewVisible(isHidden: true)
+        self.datePicker.isHidden = false
+        self.toggleButton.isSelected = false
+        
+        self.timer?.cancel()
+        self.timer = nil
     }
 }
