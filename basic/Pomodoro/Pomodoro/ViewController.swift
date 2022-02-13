505b643601f35b47f143d019c490fa0821098684
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-02-14 00:50:23 +0900

    Update label, progress bar / Add animation

--- basic/Pomodoro/Pomodoro/ViewController.swift
+++ basic/Pomodoro/Pomodoro/ViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import AudioToolbox
 
 enum TimerStatus{
     case start
@@ -12,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var toggleButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
     
     var duration = 60
     var currentSeconds = 0
@@ -38,8 +40,11 @@ class ViewController: UIViewController {
         case .end:
             self.timerStatus = .start
             self.toggleButton.isSelected = true // '일시정지'으로
-            self.setTimerInfoViewVisible(isHidden: false)
-            self.datePicker.isHidden = true
+            UIView.animate(withDuration: 0.5, animations: {
+                self.timerLabel.alpha = 1
+                self.progressView.alpha = 1
+                self.datePicker.alpha = 0
+            })
             self.cancelButton.isEnabled = true
             self.currentSeconds = self.duration
             self.startTimer()
@@ -66,12 +71,29 @@ class ViewController: UIViewController {
             self.timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
             self.timer?.schedule(deadline: .now(), repeating: 1)
             self.timer?.setEventHandler(handler: { [weak self] in
-                self?.currentSeconds -= 1
-                debugPrint(self?.currentSeconds)
-                if self?.currentSeconds ?? 0 <= 0 {
-                    self?.stopTimer()
+                guard let self = self else { return }
+                self.currentSeconds -= 1
+                debugPrint(self.currentSeconds)
+                if self.currentSeconds <= 0 {
+                    self.stopTimer()
+                    AudioServicesPlaySystemSound(1005)
                 }
+                // timer label
+                let hour = self.currentSeconds / 3600
+                let minute = (self.currentSeconds % 3600) / 60
+                let seconds = (self.currentSeconds % 3600) % 60
+                self.timerLabel.text = String(format: "%02d:%02d:%02d", hour, minute, seconds) // label
+                // progressview
+                self.progressView.progress = Float(self.currentSeconds) / Float(self.duration)
+                debugPrint(self.progressView.progress)
                 
+                // animation
+                UIView.animate(withDuration: 0.5, delay: 0, animations: {
+                    self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+                })
+                UIView.animate(withDuration: 0.5, delay: 0.5, animations: {
+                    self.imageView.transform = CGAffineTransform(rotationAngle: .pi*2)
+                })
             })
             self.timer?.resume()
         }
@@ -82,8 +104,12 @@ class ViewController: UIViewController {
         }
         self.timerStatus = .end
         self.cancelButton.isEnabled = false
-        self.setTimerInfoViewVisible(isHidden: true)
-        self.datePicker.isHidden = false
+        UIView.animate(withDuration: 0.5) {
+            self.timerLabel.alpha = 0
+            self.progressView.alpha = 0
+            self.datePicker.alpha = 1
+            self.imageView.transform = .identity
+        }
         self.toggleButton.isSelected = false
         
         self.timer?.cancel()
