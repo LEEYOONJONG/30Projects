1d72ab00b29fef64b6293f9ca7965548dcde97ad
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-02-23 15:48:50 +0900

    Add basic UI for Corona App

---
+++ basic/COVID19/COVID19/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by YOONJONG on 2022/02/23.
+//
+
+import UIKit
+import Charts
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var totalCaseLabel: UILabel!
+    @IBOutlet weak var newCaseLabel: UILabel!
+    @IBOutlet weak var pieChartView: PieChartView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
