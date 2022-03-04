6d7f9c5f3c41f3e7417ee8bf3a64d5d56509ffce
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-03-05 00:44:05 +0900

    remote config

--- intermediate/Notice/Notice/ViewController.swift
+++ intermediate/Notice/Notice/ViewController.swift
@@ -1,33 +1,54 @@
-//
-//  ViewController.swift
-//  Notice
-//
-//  Created by YOONJONG on 2022/02/26.
-//
-
 import UIKit
+import FirebaseRemoteConfig
 
 class ViewController: UIViewController {
-    var noticeContents:(title:String, detail:String, date:String)?
     
-    @IBOutlet weak var noticeView: UIView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var detailLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
-  
+   
+
+    
+    var remoteConfig:RemoteConfig?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.remoteConfig = RemoteConfig.remoteConfig()
+        let setting = RemoteConfigSettings()
+        setting.minimumFetchInterval = 0
+        
+        self.remoteConfig?.configSettings = setting
+        self.remoteConfig?.setDefaults(fromPlist: "RemoteConfigDefaults")
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        noticeView.layer.cornerRadius = 6
-        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-        guard let noticeContents = noticeContents else {
-            return
-        }
-        titleLabel.text = noticeContents.title
-        detailLabel.text = noticeContents.detail
-        dateLabel.text = noticeContents.date
-    }
-    @IBAction func doneButtonTapped(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+        getNotice()
     }
 }
-
+extension ViewController {
+    func getNotice(){
+        guard let remoteConfig = remoteConfig else { return }
+        
+        remoteConfig.fetch { [weak self] status, _ in
+            if status == .success {
+                remoteConfig.activate(completion: nil)
+            } else {
+                print("Error: Config not fetched")
+            }
+            guard let self = self else { return }
+            
+            if !self.isNoticeHidden(remoteConfig){ // 보여주기
+                let noticeVC = NoticeViewController(nibName: "NoticeViewController", bundle: nil)
+                noticeVC.modalPresentationStyle = .custom
+                noticeVC.modalTransitionStyle = .crossDissolve
+                
+                let title = (remoteConfig["title"].stringValue ?? "").replacingOccurrences(of: "\\n", with: "\n")
+                let detail = (remoteConfig["detail"].stringValue ?? "").replacingOccurrences(of: "\\n", with: "\n")
+                let date = (remoteConfig["date"].stringValue ?? "").replacingOccurrences(of: "\\n", with: "\n")
+                noticeVC.noticeContents = (title:title, detail:detail, date:date)
+                self.present(noticeVC, animated: true, completion: nil)
+            }
+        }
+        
+    }
+    func isNoticeHidden(_ remoteConfig:RemoteConfig)->Bool{
+        return remoteConfig["isHidden"].boolValue
+    }
+}
