1d24ba91fdec52d75de30319f8b51946996be207
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-03-09 22:47:53 +0900

    Add A/B testing

--- intermediate/Notice/Notice/NoticeViewController.swift
+++ intermediate/Notice/Notice/NoticeViewController.swift
@@ -9,21 +9,34 @@ import UIKit
 
 class NoticeViewController: UIViewController {
 
+    @IBOutlet weak var noticeView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var noticeContents:(title:String, detail:String, date:String)? // 못받아올수도 있으므로 옵셔널
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        noticeView.layer.cornerRadius = 6
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        guard let noticeContents = noticeContents else {
+            return
+        }
+        titleLabel.text = noticeContents.title
+        detailLabel.text = noticeContents.detail
+        dateLabel.text = noticeContents.date
     }
-    */
-
+ 
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
--- intermediate/Notice/Notice/ViewController.swift
+++ intermediate/Notice/Notice/ViewController.swift
@@ -1,16 +1,16 @@
 import UIKit
 import FirebaseRemoteConfig
+import FirebaseAnalytics
 
 class ViewController: UIViewController {
-    
-   
-
     
     var remoteConfig:RemoteConfig?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.remoteConfig = RemoteConfig.remoteConfig()
+        
         let setting = RemoteConfigSettings()
         setting.minimumFetchInterval = 0
         
@@ -44,11 +44,42 @@ extension ViewController {
                 let date = (remoteConfig["date"].stringValue ?? "").replacingOccurrences(of: "\\n", with: "\n")
                 noticeVC.noticeContents = (title:title, detail:detail, date:date)
                 self.present(noticeVC, animated: true, completion: nil)
+            } else {
+                self.showEventAlert()
             }
         }
         
     }
     func isNoticeHidden(_ remoteConfig:RemoteConfig)->Bool{
         return remoteConfig["isHidden"].boolValue
+    }
+}
+
+extension ViewController{
+    func showEventAlert(){
+        guard let remoteConfig = remoteConfig else { return }
+        
+        remoteConfig.fetch { [weak self] status, _ in
+            if status == .success {
+                remoteConfig.activate(completion: nil)
+            } else {
+                print("Config not fetched")
+            }
+            
+            let message = remoteConfig["message"].stringValue ?? ""
+            print("message : ", message)
+            
+            let confirmAction = UIAlertAction(title: "확인하기", style: .default) { _ in
+                Analytics.logEvent("promotion_alert", parameters: nil)
+            }
+            
+            let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+            let alertController = UIAlertController(title: "깜짝이벤트", message: message, preferredStyle: .alert)
+            
+            alertController.addAction(confirmAction)
+            alertController.addAction(cancelAction)
+            
+            self?.present(alertController, animated: true, completion: nil)
+        }
     }
 }
