c09f479f3ddde72f7c86c5a72a81da65e65aaa28
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-04-12 18:40:24 +0900

    Set basic setting for tabbarcontroller

---
+++ upper intermediate/SwiftUIPractice/SwiftUIPractice/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIPractice
+//
+//  Created by YOONJONG on 2022/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MyView(helloFont: .largeTitle)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ upper intermediate/SwiftUIPractice/SwiftUIPractice/MyView.swift
@@ -0,0 +1,25 @@
+//
+//  MyView.swift
+//  SwiftUIPractice
+//
+//  Created by YOONJONG on 2022/04/07.
+//
+
+import SwiftUI
+
+struct MyView: View {
+    let helloFont: Font
+    var body: some View {
+        VStack{
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .font(helloFont)
+            Text("만나서 반가워")
+        }
+    }
+}
+
+struct MyView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyView(helloFont: .subheadline)
+    }
+}
