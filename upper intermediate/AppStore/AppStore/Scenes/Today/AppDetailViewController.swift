0af0072288040895f0bba954843c8f9e61a9f470
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-04-15 00:31:17 +0900

    Add sharesheet

--- upper intermediate/AppStore/AppStore/Scenes/Today/AppDetailViewController.swift
+++ upper intermediate/AppStore/AppStore/Scenes/Today/AppDetailViewController.swift
@@ -42,6 +42,7 @@ final class AppDetailViewController:UIViewController{
         let button = UIButton()
         button.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
         button.tintColor = .systemBlue
+        button.addTarget(self, action: #selector(didTapShareButton), for: .touchUpInside)
         return button
     }()
     
@@ -96,5 +97,10 @@ private extension AppDetailViewController{
             $0.bottom.equalTo(appIconImageView.snp.bottom)
             $0.trailing.equalToSuperview().inset(16)
         }
+    }
+    @objc func didTapShareButton(){
+        let activityItems:[Any] = [today.title]
+        let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        present(activityViewController, animated: true)
     }
 }
